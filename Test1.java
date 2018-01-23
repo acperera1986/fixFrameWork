7e7561c4a9cfc7835068e7aa4acdd39c42d59169
asaperera <asaperera@virtusa.com>
2018-01-23 19:22:36 +0530

    uploaded dev1

---
+++ Test1.java
@@ -0,0 +1,10 @@
+public class Test1{
+public static void main (String[] args){
+	System.out.println("Hello People");
+	System.out.println("Dev_1");
+	System.out.println("Dev_1");
+	System.out.println("Hello People");
+	System.out.println("Hello People");
+}
+
+}
